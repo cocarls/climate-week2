0a45369829f1b50e6d0bfa7259eb2d3252c19cd2
cocarls <carloscoronadouribe@gmail.com>
2023-10-17 15:16:01 -0500

    areglando el logo del loading

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,7 +1,7 @@
 const Loading = () => {
   return (
-    <div className="bg-black absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center ">
-        <img src="/loading.jpg" alt="" />
+    <div className="bg-[url(/fondoclima.jpeg)] absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center bg-cover bg-center ">
+        <img className="rounded-full" src="/loading.jpg" alt="" />
     </div>
   )
 }
